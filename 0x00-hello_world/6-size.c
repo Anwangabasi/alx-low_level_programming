c77ac70a68564ec66bb065d00271dbd233adbb8e
Anwangabasi <udoakaanwangaabasi@gmail.com>
2021-08-30 14:51:19 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 printf(
-        "Size of a char: %d byte(s)\n"
-	"Size of an int: %d byte(s)\n"
-	"Size of a long int: %d byte(s)\n"
-	"Size of a long long int: %d bytes(s)\n"
-	"Size of a float: %d byte(s)\n"
-	sizeof(char),
-	sizeof(int),
-	sizeof(long int),
-	sizeof(long long int),
-	sizeof(float)
-        );
+         "Size of a char: %d byte(s)\n"
+	 "Size of an int: %d byte(s)\n"
+	 "Size of a long int: %d byte(s)\n"
+	 "Size of a long long int: %d bytes(s)\n"
+	 "Size of a float: %d byte(s)\n",
+	 sizeof(char),
+	 sizeof(int),
+	 sizeof(long int),
+	 sizeof(long long int),
+	 sizeof(float)
+         );
 return (0);
 }
