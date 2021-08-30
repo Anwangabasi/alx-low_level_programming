51fc2c32c39948fb4a4821870af03d4c2b070ff3
Anwangabasi <udoakaanwangaabasi@gmail.com>
2021-08-30 16:16:11 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-char err_msg[]=
+char err_msg[] =
 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fwrite(err_msg, 1, sizeof(err_msg) - 1, stderr);
 return (1);
