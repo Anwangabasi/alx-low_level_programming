0ea9a3ca81423714982ca05fa15916bd35576424
Anwangabasi <udoakaanwangaabasi@gmail.com>
2021-09-01 16:13:16 -0700

    24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ i = 0;
 while (i < 24)
 {
 j = 0;
- while (j < 60)
+while (j < 60)
 	{
 	  _putchar((i / 10) + '0');
 	  _putchar((i % 10) + '0');
@@ -23,9 +23,9 @@ j = 0;
 	  _putchar((j / 10) + '0');
 	  _putchar((j % 10) + '0');
 	  _putchar('\n');
-	  j++;
+	j++;
 	}
- i++;
+i++;
 }
 
 }
